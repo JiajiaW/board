4a57c07b350610f4127cb3600d22a28d82532596
jia <1725799902@qq.com>
2019-09-17 21:44:30 +0800

    backspace string compare add another idea

--- algorithm/src/main/java/com/jia/board/algorithm/StackAndQueue/T844BackspaceStringCompare.java
+++ algorithm/src/main/java/com/jia/board/algorithm/StackAndQueue/T844BackspaceStringCompare.java
@@ -47,6 +47,52 @@ public class T844BackspaceStringCompare {
         return true;
     }
 
+    // 和上面同一种思路
+    public boolean backspaceCompare2(String S, String T) {
+        return build(S).equals(build(T));
+    }
+
+    public String build(String S) {
+        Stack<Character> ans = new Stack();
+        for (char c: S.toCharArray()) {
+            if (c != '#')
+                ans.push(c);
+            else if (!ans.empty())
+                ans.pop();
+        }
+        return String.valueOf(ans);
+    }
+
+
+    // 从后面开始遍历，遇到有#的向前挪动相应字符的个数。
+    // 每次比较当前字符是否相当，并且注意一个字符串已经遍历完毕而另一个字符串还有别的字符
+    public boolean backspaceCompare1(String S, String T) {
+        int i = S.length() - 1, j = T.length() - 1;
+        int skipS = 0, skipT = 0;
+
+        while (i >= 0 || j >= 0) { // While there may be chars in build(S) or build (T)
+            while (i >= 0) { // Find position of next possible char in build(S)
+                if (S.charAt(i) == '#') {skipS++; i--;}
+                else if (skipS > 0) {skipS--; i--;}
+                else break;
+            }
+            while (j >= 0) { // Find position of next possible char in build(T)
+                if (T.charAt(j) == '#') {skipT++; j--;}
+                else if (skipT > 0) {skipT--; j--;}
+                else break;
+            }
+            // If two actual characters are different
+            if (i >= 0 && j >= 0 && S.charAt(i) != T.charAt(j))
+                return false;
+            // If expecting to compare char vs nothing
+            if ((i >= 0) != (j >= 0))
+                return false;
+            i--; j--;
+        }
+        return true;
+    }
+
+
     public static void main(String[] args) {
         boolean result = backspaceCompare("a##c", "#a#c");
         System.out.println(result);
